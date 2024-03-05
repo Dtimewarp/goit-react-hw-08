2334fc7ddcdeeef63c76bdf1f54b424cc7cc4509
Dtimewarp <dtimewarp@gmail.com>
2024-03-05 16:08:08 +0200

    logic work. style upd

--- src/components/BlockHeader/BlockHeader.jsx
+++ src/components/BlockHeader/BlockHeader.jsx
@@ -4,7 +4,7 @@ import css from './BlockHeader.module.css';
 export default function BlockHeader({ children }) {
   return (
     <div className={css.header}>
-      <Typography variant="body" fontSize={16} gutterBottom>
+      <Typography variant="body" fontSize={36} gutterBottom>
         {children}
       </Typography>
     </div>
